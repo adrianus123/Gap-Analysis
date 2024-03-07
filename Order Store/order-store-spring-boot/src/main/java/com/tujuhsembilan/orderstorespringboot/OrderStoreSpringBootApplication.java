53c79cdebae7dfe15541f179beb2f02e786615b7
adrianus123 <adrianussimarmata9@gmail.com>
2024-03-07 09:00:52 +0700

    Initial Project BE

---
+++ Order Store/order-store-spring-boot/src/main/java/com/tujuhsembilan/orderstorespringboot/OrderStoreSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.tujuhsembilan.orderstorespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderStoreSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderStoreSpringBootApplication.class, args);
+	}
+
+}
