af6d111ab047c020835a959345e0a3996cc920d5
adrianus123 <adrianussimarmata9@gmail.com>
2024-03-28 14:17:18 +0700

    push project

---
+++ Workflow Engine/DesignerApp/Controllers/DesignerController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OptimaJet.Workflow;
+using System.Collections.Specialized;
+using System.Text;
+using WorkflowLib;
+
+namespace DesignerApp.Controllers
+{
+    public class DesignerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Api()
+        {
+            Stream? filestream = null;
+            var parameters = new NameValueCollection();
+
+            var isPost = Request.Method.Equals("POST", StringComparison.OrdinalIgnoreCase);
+
+            foreach (var q in Request.Query)
+            {
+                parameters.Add(q.Key, q.Value.First());
+            }
+
+            if (isPost)
+            {
+                var keys = parameters.AllKeys;
+                foreach (var key in Request.Form.Keys)
+                {
+                    if (!keys.Contains(key))
+                    {
+                        parameters.Add(key, Request.Form[key]);
+                    }
+                }
+
+                if (Request.Form.Files.Count > 0)
+                {
+                    filestream = Request.Form.Files[0].OpenReadStream();
+                }
+            }
+
+            var (result, hasError) = await WorkflowInit.Runtime.DesignerAPIAsync(parameters, filestream);
+            if (parameters["operation"]?.ToLower() == "downloadscheme" && !hasError)
+            {
+                return File(Encoding.UTF8.GetBytes(result), "text/xml");
+            }
+
+            if (parameters["operation"]?.ToLower() == "downloadschemebpmn" && !hasError)
+            {
+                return File(Encoding.UTF8.GetBytes(result), "text/xml");
+            }
+
+            return Content(result);
+        }
+    }
+}
--- Workflow Engine/WorkflowLib/WorkflowInit.cs
+++ Workflow Engine/WorkflowLib/WorkflowInit.cs
@@ -1,9 +1,41 @@
-﻿using System;
+﻿using OptimaJet.Workflow.Core.Builder;
+using OptimaJet.Workflow.Core.Parser;
+using OptimaJet.Workflow.Core.Runtime;
+using OptimaJet.Workflow.DbPersistence;
+using OptimaJet.Workflow.Plugins;
+using System;
+using System.Xml.Linq;
 
 namespace WorkflowLib
 {
     public class WorkflowInit
     {
+        private static readonly Lazy<WorkflowRuntime> LazyRuntime = new Lazy<WorkflowRuntime>(InitWorkflowRuntime);
+        public static WorkflowRuntime Runtime { get { return LazyRuntime.Value; } }
+        public static string ConnectionString { get; set; }
+        private static WorkflowRuntime InitWorkflowRuntime()
+        {
+            if (string.IsNullOrEmpty(ConnectionString))
+            {
+                throw new Exception("Please init ConnectionString before calling the Runtime!");
+            }
 
+            var dbProvider = new MSSQLProvider(ConnectionString);
+            var builder = new WorkflowBuilder<XElement>(dbProvider, new XmlWorkflowParser(), dbProvider).WithDefaultCache();
+            var runtime = new WorkflowRuntime()
+                .WithBuilder(builder)
+                .WithPersistenceProvider(dbProvider)
+                .EnableCodeActions()
+                .SwitchAutoUpdateSchemeBeforeGetAvailableCommandsOn()
+                .AsSingleServer();
+
+            var plugin = new BasicPlugin();
+            runtime.WithPlugin(plugin);
+            runtime.ProcessActivityChanged += (sender, args) => { };
+            runtime.ProcessStatusChanged += (sender, args) => { };
+            runtime.Start();
+
+            return runtime;
+        }
     }
 }
