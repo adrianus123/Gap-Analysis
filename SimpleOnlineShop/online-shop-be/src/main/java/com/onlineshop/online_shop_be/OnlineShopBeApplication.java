abda5c8caeb6e7258878bac3a96c1b28603357a2
adrianus123 <adrianussimarmata9@gmail.com>
2024-05-27 13:32:03 +0700

    initial commit

---
+++ SimpleOnlineShop/online-shop-be/src/main/java/com/onlineshop/online_shop_be/OnlineShopBeApplication.java
@@ -0,0 +1,13 @@
+package com.onlineshop.online_shop_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineShopBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineShopBeApplication.class, args);
+	}
+
+}
